a70a0b124fb1d439545a39d8920c30a006e77b68
yinoue <yinoue@densan-soft.co.jp>
2015-04-20 20:10:12 +0900

    edit package.json

--- app.js
+++ app.js
@@ -1,36 +1,48 @@
-// 1.モジュールオブジェクトの初期化
+// サーバー構築
+var http = require("http");
+// fsモジュールの読み込み
 var fs = require("fs");
-var server = require("http").createServer(function(req, res) {
-     res.writeHead(200, {"Content-Type":"text/html"});
-     var output = fs.readFileSync("./index.html", "utf-8");
-     res.end(output);
-}).listen(8080);
-var io = require("socket.io").listen(server);
+// pathモジュールの読み込み
+var path = require("path");
+// httpサーバーを立てる
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type":"text/html"});
+    var output = fs.readFileSync("./index.html", "utf-8");
+    res.end(output);
+});
 
-// ユーザ管理ハッシュ
-var userHash = {};
+// httpサーバーを起動する。
+server.listen((process.env.PORT || 5000), function() {
+    console.log((process.env.PORT || 5000) + "でサーバーが起動しました");
+});
 
-// 2.イベントの定義
-io.sockets.on("connection", function (socket) {
+
+// socket.ioの読み込み
+var socket = require("socket.io");
+// サーバーでSocket.IOを使える状態にする
+var io = socket.listen(server);
+
+io.sockets.on("connection", function (sock) {
 
   // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
-  socket.on("connected", function (name) {
+  sock.on("connected", function (name) {
     var msg = name + "が入室しました";
-    userHash[socket.id] = name;
+    userHash[sock.id] = name;
     io.sockets.emit("publish", {value: msg});
   });
 
   // メッセージ送信カスタムイベント
-  socket.on("publish", function (data) {
+  sock.on("publish", function (data) {
     io.sockets.emit("publish", {value:data.value});
   });
 
   // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
-  socket.on("disconnect", function () {
-    if (userHash[socket.id]) {
-      var msg = userHash[socket.id] + "が退出しました";
-      delete userHash[socket.id];
+  sock.on("disconnect", function () {
+    if (userHash[sock.id]) {
+      var msg = userHash[sock.id] + "が退出しました";
+      delete userHash[sock.id];
       io.sockets.emit("publish", {value: msg});
     }
   });
 });
+
